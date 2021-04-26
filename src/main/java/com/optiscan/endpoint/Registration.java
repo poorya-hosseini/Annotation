1196a91b1a9fcf6b111d9a071b49f114e10dbc6b
Pooria Hosseini <pooria.hosseini@axelhealth.com>
2021-04-26 18:37:41 +0300

    Replaced mysql with postgres

--- src/main/java/com/optiscan/endpoint/Registration.java
+++ src/main/java/com/optiscan/endpoint/Registration.java
@@ -1,7 +1,7 @@
 package com.optiscan.endpoint;
 
-import com.optiscan.entity.Person;
-import com.optiscan.service.PersonService;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,36 +13,53 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.validation.Valid;
+import com.optiscan.entity.Person;
+import com.optiscan.service.PersonService;
 
 @Controller
 public class Registration extends WebMvcConfigurerAdapter {
 
-    @Autowired
-    private PersonService service;
+	@Autowired
+	private PersonService service;
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/confirm").setViewName("confirm");
-        registry.addViewController("/registration").setViewName("registration");
-    }
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/confirm").setViewName("confirm");
+		registry.addViewController("/registration").setViewName("registration");
+	}
 
-    @GetMapping("/")
-    public String registration(Model model) {
-        model.addAttribute("person", new Person());
-        return "registration";
-    }
+	@GetMapping("/")
+	public String index(Model model) {
+		return "redirect:/registration";
+	}
 
-    @PostMapping("/")
-    public ModelAndView save(@Valid @ModelAttribute Person person, BindingResult bindingResult, ModelAndView mav, final RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            mav.setViewName("registration");
-            return mav;
-        }
-        service.save(person);
-        redirectAttributes.addFlashAttribute("saved", person);
-        mav.setViewName("redirect:/confirm");
-        return mav;
-    }
+	@GetMapping("/registration")
+	public String registration(Model model) {
+		model.addAttribute("person", new Person());
+		return "registration";
+	}
+
+	@GetMapping("/confirm")
+	public String confirm(Model model) {
+
+		model.addAttribute("person", new Person());
+		if (model.containsAttribute("saved")) {
+			return "confirm";
+		} else {
+			return "redirect:/registration";
+		}
+	}
+
+	@PostMapping("/")
+	public ModelAndView save(@Valid @ModelAttribute Person person, BindingResult bindingResult, ModelAndView mav,
+		final RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			mav.setViewName("redirect:/registration");
+			return mav;
+		}
+		service.save(person);
+		redirectAttributes.addFlashAttribute("saved", person);
+		mav.setViewName("redirect:/confirm");
+		return mav;
+	}
 }
